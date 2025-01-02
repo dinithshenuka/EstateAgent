9fb025b3bcfaeda36dce50ade6a10ca59c0281ea
Dinith Perera <dinithshenuka@gmail.com>
2025-01-02 06:57:36 +0530

    nav bar responsive

--- estate-agent/src/components/Navbar.jsx
+++ estate-agent/src/components/Navbar.jsx
@@ -1,53 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch, faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faHeart, faBars } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <div className="navbar-left">
-          <span className="navbar-brand">Estate Agent</span>
-        </div>
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
 
-        <div className="collapse navbar-collapse" id="navbarNav">
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+      <div className="container">
+        {/* Brand */}
+        <Link className="navbar-brand fw-bold" to="/">
+          Estate Agent
+        </Link>
+
+        {/* Toggle Button */}
+        <button
+          className="navbar-toggler border-0"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded={!isNavCollapsed}
+          aria-label="Toggle navigation"
+          onClick={handleNavCollapse}
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+
+        {/* Navigation Items */}
+        <div
+          className={`${isNavCollapsed ? "collapse" : ""} navbar-collapse`}
+          id="navbarNav"
+        >
+          {/* Center Menu */}
           <ul className="navbar-nav mx-auto">
-            <li className="nav-item">
+            <li className="nav-item px-2">
               <Link className="nav-link" to="/">
                 Home
               </Link>
             </li>
-
-            <li className="nav-item">
+            <li className="nav-item px-2">
               <Link className="nav-link" to="/agents">
                 Agents
               </Link>
             </li>
-
-            <li className="nav-item">
+            <li className="nav-item px-2">
               <Link className="nav-link" to="/about">
                 About
               </Link>
             </li>
-
-            <li className="nav-item">
+            <li className="nav-item px-2">
               <Link className="nav-link" to="/contact">
                 Contact Us
               </Link>
             </li>
           </ul>
 
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/search">
-                <FontAwesomeIcon icon={faSearch} className="icon-search" />{" "}
+          {/* Right Menu */}
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item px-2">
+              <Link className="nav-link d-flex align-items-center" to="/search">
+                <FontAwesomeIcon icon={faSearch} className="me-2" />
                 Search
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/favorites">
-                <FontAwesomeIcon icon={faHeart} className="icon-favorite" />{" "}
+            <li className="nav-item px-2">
+              <Link
+                className="nav-link d-flex align-items-center"
+                to="/favorites"
+              >
+                <FontAwesomeIcon icon={faHeart} className="me-2" />
                 Favorites
               </Link>
             </li>
