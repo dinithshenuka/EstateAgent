8508617fbef82998ac41aa19e609ffe1ff16c2cd
Dinith Perera <dinithshenuka@gmail.com>
2025-01-02 07:12:57 +0530

    footer

--- estate-agent/src/components/Footer.jsx
+++ estate-agent/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFacebookF,
@@ -15,118 +16,150 @@ import {
 
 function Footer() {
   return (
-    <footer className="navbar-light bg-light mt-5 py-4">
+    <footer className="bg-light py-5">
       <div className="container">
+        {/* Main Content */}
         <div className="row gy-4">
-          <div className="col-lg-3 col-md-6">
-            <div className="navbar-brand mb-3">Estate Agent</div>
-            <p className="text-muted">
+          {/* Company Info */}
+          <div className="col-12 col-sm-6 col-lg-3">
+            <h5 className="mb-3 text-center text-sm-start">Estate Agent</h5>
+            <p className="text-muted text-center text-sm-start">
               Your trusted partner in finding the perfect property. We help
               connect buyers with their dream homes and sellers with qualified
               prospects.
             </p>
           </div>
 
-          <div className="col-lg-3 col-md-6">
-            <h6 className="nav-link mb-3">Property Types</h6>
-            <ul className="nav flex-column">
-              <li className="nav-item">
-                <a href="#!" className="nav-link px-0 text-black">
+          {/* Property Types */}
+          <div className="col-12 col-sm-6 col-lg-3">
+            <h5 className="mb-3 text-center text-sm-start">Property Types</h5>
+            <ul className="list-unstyled text-center text-sm-start">
+              <li className="mb-2">
+                <Link className="nav-link" to="/properties/houses">
                   Houses
-                </a>
+                </Link>
               </li>
-              <li className="nav-item">
-                <a href="#!" className="nav-link px-0 text-black">
+              <li className="mb-2">
+                <Link className="nav-link" to="/properties/apartments">
                   Apartments
-                </a>
+                </Link>
               </li>
-              <li className="nav-item">
-                <a href="#!" className="nav-link px-0 text-black">
+              <li className="mb-2">
+                <Link className="nav-link" to="/properties/land">
                   Land
-                </a>
+                </Link>
               </li>
-              <li className="nav-item">
-                <a href="#!" className="nav-link px-0 text-black">
+              <li className="mb-2">
+                <Link className="nav-link" to="/properties/commercial">
                   Commercial
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
 
-          <div className="col-lg-3 col-md-6">
-            <h6 className="nav-link mb-3">Quick Links</h6>
-            <ul className="nav flex-column">
-              <li className="nav-item">
-                <a href="/search" className="nav-link px-0 text-black">
+          {/* Quick Links */}
+          <div className="col-12 col-sm-6 col-lg-3">
+            <h5 className="mb-3 text-center text-sm-start">Quick Links</h5>
+            <ul className="list-unstyled text-center text-sm-start">
+              <li className="mb-2">
+                <Link className="nav-link" to="/search">
                   Search Properties
-                </a>
+                </Link>
               </li>
-              <li className="nav-item">
-                <a href="/favorites" className="nav-link px-0 text-black">
+              <li className="mb-2">
+                <Link className="nav-link" to="/favorites">
                   My Favorites
-                </a>
+                </Link>
               </li>
-              <li className="nav-item">
-                <a href="/agents" className="nav-link px-0 text-black">
-                  Our Agents
-                </a>
+              <li className="mb-2">
+                <Link className="nav-link" to="/about">
+                  About Us
+                </Link>
               </li>
-              <li className="nav-item">
-                <a href="/contact" className="nav-link px-0 text-black">
-                  Contact Us
-                </a>
+              <li className="mb-2">
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
               </li>
             </ul>
           </div>
 
-          <div className="col-lg-3 col-md-6">
-            <h6 className="nav-link mb-3">Contact</h6>
-            <div className="text-muted">
-              <p>
-                <FontAwesomeIcon icon={faHome} className="me-2" />
-                123 Property Lane, Real Estate City
-              </p>
-              <p>
-                <FontAwesomeIcon icon={faEnvelope} className="me-2" />
-                contact@estateagent.com
-              </p>
-              <p>
-                <FontAwesomeIcon icon={faPhone} className="me-2" />
-                (033) 123-4567
-              </p>
-              <p>
-                <FontAwesomeIcon icon={faClock} className="me-2" />
-                Mon-Fri: 9:00 AM - 6:00 PM
-              </p>
-            </div>
+          {/* Contact Info */}
+          <div className="col-12 col-sm-6 col-lg-3">
+            <h5 className="mb-3 text-center text-sm-start">Contact</h5>
+            <ul className="list-unstyled text-center text-sm-start">
+              <li className="mb-2">
+                <span className="text-muted">
+                  <FontAwesomeIcon icon={faHome} className="me-2" />
+                  123 Property Lane, Real Estate City
+                </span>
+              </li>
+              <li className="mb-2">
+                <span className="text-muted">
+                  <FontAwesomeIcon icon={faEnvelope} className="me-2" />
+                  contact@estateagent.com
+                </span>
+              </li>
+              <li className="mb-2">
+                <span className="text-muted">
+                  <FontAwesomeIcon icon={faPhone} className="me-2" />
+                  (033) 123-4567
+                </span>
+              </li>
+              <li className="mb-2">
+                <span className="text-muted">
+                  <FontAwesomeIcon icon={faClock} className="me-2" />
+                  Mon-Fri: 9:00 AM - 6:00 PM
+                </span>
+              </li>
+            </ul>
           </div>
         </div>
 
         <hr className="my-4" />
 
-        <div className="row align-items-center">
-          <div className="col-md-8 text-muted">
-            © {new Date().getFullYear()} Estate Agent. All rights reserved.
+        {/* Bottom Footer */}
+        <div className="row">
+          <div className="col-12 col-sm-6 text-center text-sm-start mb-3 mb-sm-0">
+            <span className="text-muted">
+              © {new Date().getFullYear()} Estate Agent. All rights reserved.
+            </span>
           </div>
-          <div className="col-md-4">
-            <ul className="nav justify-content-end">
-              <li className="nav-item">
-                <a href="https://facebook.com" className="nav-link text-muted">
+          <div className="col-12 col-sm-6">
+            <ul className="list-inline mb-0 text-center text-sm-end">
+              <li className="list-inline-item">
+                <a
+                  href="https://facebook.com"
+                  className="nav-link px-2"
+                  aria-label="Facebook"
+                >
                   <FontAwesomeIcon icon={faFacebookF} />
                 </a>
               </li>
-              <li className="nav-item">
-                <a href="https://twitter.com" className="nav-link text-muted">
+              <li className="list-inline-item">
+                <a
+                  href="https://twitter.com"
+                  className="nav-link px-2"
+                  aria-label="Twitter"
+                >
                   <FontAwesomeIcon icon={faTwitter} />
                 </a>
               </li>
-              <li className="nav-item">
-                <a href="https://instagram.com" className="nav-link text-muted">
+              <li className="list-inline-item">
+                <a
+                  href="https://instagram.com"
+                  className="nav-link px-2"
+                  aria-label="Instagram"
+                >
                   <FontAwesomeIcon icon={faInstagram} />
                 </a>
               </li>
-              <li className="nav-item">
-                <a href="https://linkedin.com" className="nav-link text-muted">
+              <li className="list-inline-item">
+                <a
+                  href="https://linkedin.com"
+                  className="nav-link px-2"
+                  aria-label="LinkedIn"
+                >
                   <FontAwesomeIcon icon={faLinkedin} />
                 </a>
               </li>
